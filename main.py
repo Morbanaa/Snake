6c41c55858fd9dfc020eb9ba62ab64f5be8344b7
Morbanaa <theodorerodd@gmail.com>
2026-05-19 21:54:38 -0500

    Created main loop calling world creation and rendering it each frame. Game runs at consistent rate based on game_speed

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-ECHO is on.
+# Teddy Rodd
+# Morbanaa Studios
+# Snake
+
+import time
+from game_manager import Game_Manager
+
+def main():
+    game_speed = 0.5
+    game_manager = Game_Manager(25,50)
+    game_manager.world_gen()
+    while True:
+        game_manager.render_world()
+        game_manager.clear_move_cursor()
+        time.sleep(game_speed)
+
+if __name__ == "__main__":
+    main()
